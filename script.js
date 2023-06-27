cda9034f87a5c6a0855fa5a70545be476dbfab12
Sumit Rajpurohit <118529312+SumitRajpurohit@users.noreply.github.com>
2023-06-27 14:12:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,5 @@ function getFormvalue() {
 	var firstname=document.getElementById("fname").value;
 	var lastname=document.getElementById("lname").value;
 
-	alert(firstname+ " " + lastname);
-
-	
-
+	alert(firstname+ " " + lastname);
 }
